fe8ff56bd9a979ed993da9afbc8b936c2e6ea58b
Popa Vlad <popa.vlad94@gmail.com>
2023-08-19 10:59:20 +0300

    testing success message

---
+++ utils/diamond-storage.ts
@@ -0,0 +1,75 @@
+import { execSync } from "child_process";
+import axios from "axios";
+import fs from "fs";
+import _ from "lodash";
+import path from "path";
+
+const facetsFolder = "./src/dollar/facets";
+const libsFolder = "./src/dollar/libraries";
+const targetFolder = "../packages/contracts";
+
+const executeCommand = (command) => {
+  try {
+    const output = execSync(command);
+    return output.toString();
+  } catch (error) {
+    console.error(`Error executing command: ${command}`);
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+if (fs.existsSync(targetFolder)) {
+  process.chdir(targetFolder);
+} else {
+  console.error("Target folder does not exist.");
+}
+
+let fileNames = []; // Variable to store the file names
+
+function getFileNamesFromFolder(folderPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      fileNames = files.map((file) => file.split(".")[0]);
+      resolve(fileNames);
+    });
+  });
+}
+
+getFileNamesFromFolder(facetsFolder)
+  .then(() => {
+    const branchName = executeCommand("git rev-parse --abbrev-ref HEAD").replace(/[\n\r\s]+$/, "");
+    let prStorageOutput = "";
+    let devStorageOutput = "";
+
+    let storageOutputString;
+
+    for (let i = 0; i < fileNames.length; i++) {
+      const fileName = fileNames[i];
+
+      const storageOutput = executeCommand("forge inspect " + fileName + " storage --pretty");
+      storageOutputString = JSON.stringify(storageOutput);
+
+      if (branchName === "development") {
+        devStorageOutput += storageOutputString;
+      } else {
+        prStorageOutput += storageOutputString;
+      }
+    }
+
+    if (branchName === "development") {
+      fs.writeFileSync("dev_storage_output.txt", devStorageOutput);
+    } else {
+      fs.writeFileSync("pr_storage_output.txt", prStorageOutput);
+    }
+  })
+  .catch((err) => {
+    console.error("Error:", err);
+  });
+
+console.log("Waiting for storage output...");
---
+++ utils/libs-storage.ts
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+import { execSync } from "child_process";
+
+const libsFolder = "../packages/contracts/src/dollar/libraries";
+
+const executeCommand = (command) => {
+  try {
+    const output = execSync(command);
+    return output.toString();
+  } catch (error) {
+    console.error(`Error executing command: ${command}`);
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+fs.readdir(libsFolder, (err, files) => {
+  if (err) {
+    console.error('Error reading the directory:', err);
+    return;
+  }
+
+  const fileNames = files
+    .filter((file) => fs.statSync(path.join(libsFolder, file)).isFile())
+    .sort((a, b) => a.localeCompare(b));
+  let i = 0;
+  console.log("File names: " + fileNames);
+
+  const branchName = executeCommand("git rev-parse --abbrev-ref HEAD").replace(/[\n\r\s]+$/, "");
+
+  fileNames.forEach((fileName) => {
+    const filePath = path.join(libsFolder, fileName);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading the file:', err);
+        return;
+      }
+
+      const dataArray = data.split('\n');
+
+      let insideStruct = false;
+      let currentStruct = '';
+
+      for (const line of dataArray) {
+        // Remove comments starting with "//" until the end of the line
+        const lineWithoutComments = line.replace(/\/\/.*$/, '').trim();
+
+        if (insideStruct) {
+          if (lineWithoutComments === '}') {
+            // Check if the line ends with "}"
+            insideStruct = false;
+            i++;
+
+            // Write to files based on the branchName condition
+            const fileName = branchName === 'development' ?
+              'dev_libs_storage_output_' + i + '.txt' :
+              'pr_libs_storage_output_' + i + '.txt';
+
+            fs.writeFileSync(fileName, currentStruct);
+
+            currentStruct = ''; // Reset the currentStruct for the next struct block
+          } else {
+            currentStruct += ' ' + lineWithoutComments;
+          }
+        } else if (lineWithoutComments.startsWith('struct ')) {
+          insideStruct = true;
+          currentStruct = lineWithoutComments;
+        }
+      }
+    });
+  });
+});
